a35b3a020903e3bfad37cef751d6b06b74132382
Yuri Kuznetsov <yyktomtom@gmail.com>
2017-12-14 19:28:29 -0600

    Added main window of doteditor

--- examples/doteditor/main.cc
+++ examples/doteditor/main.cc
@@ -2,11 +2,21 @@
 //
 //
 
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
 
-#include <iostream>
+#include "mainwindow.h"
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	//Q_INIT_RESOURCE(application);
+
+	QApplication app(argc, argv);
+	MainWindow mainWin;
+
+	mainWin.show();
+	return app.exec();
 }
 
 // end of file
