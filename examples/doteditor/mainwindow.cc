ea73fdb8ea8f030394a3cfd61eb2380b888b0827
Yury Kuznetsov <yyktomtom@gmail.com>
2017-12-15 11:30:06 -0600

    Some modifications to doteditor

--- examples/doteditor/mainwindow.cc
+++ examples/doteditor/mainwindow.cc
@@ -5,6 +5,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "rasterwindow.h"
+
+class SimpleRasterEditor : public RasterWindow {
+public:
+    SimpleRasterEditor(QWindow *parent = 0) : RasterWindow(parent) {}
+    void render(QPainter *p) override
+    {
+    }
+};
+
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
