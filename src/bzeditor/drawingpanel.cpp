f4d3a24fbed2d7a3db82124e2c927288731f763b
yyk99 <yyktomtom@gmail.com>
2018-04-08 09:57:27 -0500

    Copied doteditor as initial src/bzeditor, CMakeLists.txt files were updated accordingly. (#2)

---
+++ src/bzeditor/drawingpanel.cpp
@@ -0,0 +1,176 @@
+//
+//
+//
+
+#include "drawingpanel.h"
+#include "bezierMk2.h"
+
+#include <iostream>
+#include <list>
+
+#include <QPainter>
+#include <QDebug>
+#include <QAction>
+#include <QMenu>
+#include <QMouseEvent>
+#include <QPoint>
+
+class Model {
+public:
+	Model() {
+
+	}
+
+	void addClick(QPoint const &p)
+	{
+		m_clicks.push_back(p);
+	}
+
+	void clear() {
+		m_clicks.clear();
+	}
+
+	std::list<QPoint>::const_iterator begin() const { 
+		return m_clicks.begin(); 
+	}
+	std::list<QPoint>::const_iterator end() const { 
+		return m_clicks.end(); 
+	}
+	size_t size() const {
+		return m_clicks.size();
+	}
+private:
+	std::list<QPoint> m_clicks;
+};
+
+DrawingPanel::DrawingPanel(QWidget *papa /*= 0*/) : QWidget(papa), m_model()
+{
+	createActions();
+
+	m_savedCursor = this->cursor();
+	m_model = new Model();
+}
+
+DrawingPanel::~DrawingPanel()
+{
+	delete m_model;
+}
+
+void DrawingPanel::startOver()
+{
+	m_model->clear();
+	update();
+}
+
+void DrawingPanel::resizeEvent(QResizeEvent *evnt)
+{
+	qDebug() << "Resize event...";
+	QWidget::resizeEvent(evnt);
+}
+
+void DrawingPanel::paintEvent(QPaintEvent *event)
+{
+	qDebug() << "w =" << this->width() << "h =" << this->height();
+
+	QPainter painter(this);
+
+	painter.setPen(Qt::red);
+	int margin = 0;
+	painter.drawRect(margin, margin, this->width() - 2*margin - 1, this->height() - 2*margin - 1);
+
+	painter.setPen(Qt::blue);
+	for (QPoint p : *m_model) {
+		painter.drawPoint(p);
+		painter.drawRect(QRect(p - QPoint(2,2), p + QPoint(2,2)));
+	}
+
+	{
+		QPainterPath path;
+		int count = 0;
+		for (QPoint p : *m_model) {
+			++count;
+			if (count == 1)
+				path.moveTo(p);
+			else
+				path.lineTo(p);
+		}
+		painter.setPen(Qt::black);
+		painter.drawPath(path);
+	}
+
+	if(m_model->size() > 2){
+		const int N = 300;
+		std::vector<point2d_t> controls; controls.reserve(m_model->size());
+		for (QPoint p : *m_model)
+			controls.push_back(point2d_t(p.x(), p.y()));
+
+		QPainterPath path;
+		for (int i = 0; i <= N; ++i) {
+			double u = 1.0 / N * i;
+			QPointF p = make_QPointF(bezierMk2(u, controls));
+			if (i == 0)
+				path.moveTo(p);
+			else
+				path.lineTo(p);
+		}
+		painter.setPen(Qt::green);
+		painter.drawPath(path);
+	}
+}
+
+void DrawingPanel::enterEvent(QEvent *event)
+{
+	qDebug() << "Enter...";
+	m_savedCursor = cursor();
+	setCursor(Qt::CrossCursor);
+}
+
+void DrawingPanel::leaveEvent(QEvent *event)
+{
+	qDebug() << "Leaving...";
+	setCursor(m_savedCursor);
+}
+
+void DrawingPanel::mousePressEvent(QMouseEvent *e)
+{
+	if (e->button() == Qt::MouseButton::LeftButton) {
+		m_model->addClick(e->pos());
+		update();
+	}
+}
+
+void DrawingPanel::mouseDoubleClickEvent(QMouseEvent *e)
+{
+	m_model->clear();
+	update();
+}
+
+void DrawingPanel::contextMenuEvent(QContextMenuEvent *e)
+{
+	qDebug() << "contextMenuEvent...";
+
+	QMenu menu(this);
+	menu.addAction(m_actionEndLine);
+	menu.addAction(m_actionEndLine);
+	menu.addAction(m_actionEndLine);
+	menu.exec(e->globalPos());
+}
+
+QPointF DrawingPanel::make_QPointF(point2d_t const &p)
+{
+	return QPointF(std::get<0>(p), std::get<1>(p));
+}
+
+void DrawingPanel::createActions()
+{
+	m_actionEndLine = new QAction(tr("End"), this);
+	m_actionEndLine->setShortcuts(QKeySequence::Cut);
+	m_actionEndLine->setStatusTip(tr("Cut the current selection's contents to the "
+		"clipboard"));
+	connect(m_actionEndLine, &QAction::triggered, this, &DrawingPanel::on_actionEndLine_triggered);
+}
+
+void DrawingPanel::on_actionEndLine_triggered()
+{
+	qDebug() << "on_actionEndLine_triggered...";
+}
